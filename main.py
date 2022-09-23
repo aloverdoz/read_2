7d9b580cf02b81f08872f9e7509673ac256e22e8
Vladislav Ivanishchev <aloverdoz1135852@gmail.com>
2022-09-23 18:06:45 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+cook_book = {}
+
+with open('date.txt') as file:
+    for line in file:
+        ingridients = []
+        name = line.replace('\n', '')
+        emp_count = file.readline()
+        for i in range(int(emp_count)):
+            emp = file.readline().replace('\n','')
+            ing, mass, wtf = emp.split(' | ')
+            lst = {}
+            lst['ingredient_name'] = ing
+            lst['quantity'] = mass
+            lst['measure'] = wtf
+            ingridients.append(lst)
+        cook_book[name] = ingridients
+        file.readline()
+
+def cook(dishes, person_count):
+    eat = []
+    for name, ingr in cook_book.items():
+        if name == dishes:
+            for ing in ingr:
+                app = []
+                app.append(ing['ingredient_name'])
+                app.append(int(ing['quantity']) * person_count)
+                app.append(ing['measure'])
+                eat.append(app)
+    print(eat)
+
+def get_shop_list_by_dishes(dishes, person_count):
+    if type(dishes) == str:
+        cook(dishes, person_count)
+    if type(dishes) == list:
+        for i in dishes:
+            cook(i, person_count)
+
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
